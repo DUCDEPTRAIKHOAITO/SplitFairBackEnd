71fcb649493920d5c62c6f73cec0c8be645c978b
minhduc\minhduc <minhducvo140201@gmail.com>
2025-11-09 11:05:59 +0700

    commit test

--- src/main/java/com/anygroup/splitfair/SplitfairApplication.java
+++ src/main/java/com/anygroup/splitfair/SplitfairApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SplitfairApplication {
 
+    //test
+
 	public static void main(String[] args) {
 		SpringApplication.run(SplitfairApplication.class, args);
 	}
